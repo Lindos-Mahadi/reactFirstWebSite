3f4097154407a7f2d8e1e48c5160ad2f1c782d25
lindos-mahadi <lindosmahadi@gmail.com>
2020-09-14 15:06:33 +0600

    Updates

---
+++ precache-manifest.9cc4e1a2cadd3aea8c8d7b79d238b46a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6dbee5b6831cf4330cbf27c8402e1e4",
+    "url": "/reactFirstWebSite/index.html"
+  },
+  {
+    "revision": "7d69a642f0b8ea07c324",
+    "url": "/reactFirstWebSite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "0814158652ad011e76bf",
+    "url": "/reactFirstWebSite/static/css/main.9340cedb.chunk.css"
+  },
+  {
+    "revision": "7d69a642f0b8ea07c324",
+    "url": "/reactFirstWebSite/static/js/2.2eac8a1c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactFirstWebSite/static/js/2.2eac8a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0814158652ad011e76bf",
+    "url": "/reactFirstWebSite/static/js/main.7d7e3f46.chunk.js"
+  },
+  {
+    "revision": "0d3234ffb9f5eec3abbb",
+    "url": "/reactFirstWebSite/static/js/runtime-main.adc26207.js"
+  },
+  {
+    "revision": "fe33d6ef7377f7782667a094e3df0c07",
+    "url": "/reactFirstWebSite/static/media/about.fe33d6ef.jpg"
+  },
+  {
+    "revision": "33eba52268744f9766a14daee49ca60e",
+    "url": "/reactFirstWebSite/static/media/friend1.33eba522.png"
+  },
+  {
+    "revision": "ea765aa48571407a8191c3c18f9ca217",
+    "url": "/reactFirstWebSite/static/media/friend2.ea765aa4.png"
+  },
+  {
+    "revision": "3ab8efc461b7287896466da48c174d91",
+    "url": "/reactFirstWebSite/static/media/friend3.3ab8efc4.png"
+  },
+  {
+    "revision": "18c2ab60a8c6c0545c197618758dd69c",
+    "url": "/reactFirstWebSite/static/media/rockets.18c2ab60.png"
+  }
+]);
